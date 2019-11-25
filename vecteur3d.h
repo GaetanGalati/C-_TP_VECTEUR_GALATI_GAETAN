7b4729b0933b240cd740e3ae3f29523d3bb81e6a
Galati Gaetan <skyfulle68@gmail.com>
2019-11-25 16:39:56 +0100

    EX1 - c

--- vecteur3d.h
+++ vecteur3d.h
@@ -6,7 +6,12 @@ class vecteur3d
 	float z;
 public:
 	//vecteur3d();//Constructeur 
-	vecteur3d(float = 0.0, float = 0.0, float = 0.0);//Asignation
+	vecteur3d(float X = 0.0, float Y = 0.0, float Z = 0.0)//Asignation
+	{
+		x = X;
+		y = Y;
+		z = Z;
+	}
 	~vecteur3d();
 };
 
