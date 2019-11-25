ec1bca59ef48e89de07b9061e8298b48369b3c10
Galati Gaetan <skyfulle68@gmail.com>
2019-11-25 14:05:51 +0100

    EX 1 - a

---
+++ vecteur3d.cpp
@@ -0,0 +1,22 @@
+#include "vecteur3d.h"
+
+
+
+vecteur3d::vecteur3d()//Constructeur
+{
+	x = 0; 
+	y = 0; 
+	z = 0;
+}
+
+vecteur3d::vecteur3d(float X, float Y, float Z)
+{
+	x = X; 
+	y = Y; 
+	z = Z;
+}
+
+
+vecteur3d::~vecteur3d()
+{
+}
---
+++ vecteur3d.h
@@ -0,0 +1,12 @@
+#pragma once
+class vecteur3d
+{
+	float x;
+	float y; 
+	float z;
+public:
+	vecteur3d();//Constructeur 
+	vecteur3d(float, float, float);//Asignation
+	~vecteur3d();
+};
+
