82bde1604270ba0d6f8bd9c6a9ade9c9dd16c652
Galati Gaetan <skyfulle68@gmail.com>
2019-11-25 16:51:35 +0100

    EX 2 - b

--- vecteur3d.cpp
+++ vecteur3d.cpp
@@ -1,16 +1,18 @@
 #include "vecteur3d.h"
 
 
-
+/*
 vecteur3d::vecteur3d(float X, float Y, float Z)
 {
 	x = X; 
 	y = Y; 
 	z = Z;
 }
-bool vecteur3d::coincidance(vecteur3d *vect)
+*/
+
+bool vecteur3d::coincidance(vecteur3d & vect)
 {
-	if ((vect->x == x) && (vect->y == y) && (vect->z == z)) {
+	if ((vect.x == x) && (vect.y == y) && (vect.z == z)) {
 		return true;//Coincidance ! 
 	}
 	else {
